0c3bbbf75f71917055b020ea46c9592358635750
Max Filenko <maksym.filenko@daimler.com>
2019-04-27 17:28:47 +0200

    :white_check_mark: Introduce handlers

--- db.go
+++ db.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 
@@ -27,4 +30,98 @@ func OpenDB(config *Conf) {
 
 func CloseDB() {
 	db.Close()
+}
+
+// Transform JSON to a database driver compatible type.
+func (attributes PaymentAttributes) Value() (driver.Value, error) {
+	obj, err := json.Marshal(attributes)
+	return obj, err
+}
+
+// Transform `JSONB ([]byte)` that comes from a database into our type.
+func (attributes *PaymentAttributes) Scan(data interface{}) error {
+	source, ok := data.([]byte)
+	if !ok {
+		return errors.New("Database returned unexpected data.")
+	}
+
+	return json.Unmarshal(source, &attributes)
+}
+
+// GET /payments
+func QueryPayments(payments *[]Payment) error {
+	query := `
+		SELECT
+			payments.id, payments.version, payments.organisation_id, payments.attributes,
+			payment_types.type
+		FROM
+			payments
+		INNER JOIN
+			payment_types
+		ON
+			payments.type_id = payment_types.id`
+
+	return db.Select(payments, query)
+}
+
+// GET /payments/:id
+func QueryPayment(payment *Payment, id string) error {
+	query := `
+		SELECT
+			payments.id, payments.version, payments.organisation_id, payments.attributes,
+			payment_types.type
+		FROM
+			payments
+		INNER JOIN
+			payment_types
+		ON
+			payments.type_id = payment_types.id
+		WHERE
+			payments.id = $1`
+
+	return db.Get(payment, query, id)
+}
+
+// POST /payments
+func StorePayment(payment *Payment) error {
+	query := `
+		INSERT INTO
+			payments (type_id, organisation_id, attributes)
+		VALUES
+			((SELECT id FROM payment_types WHERE type = $1), $2, $3)
+		RETURNING
+			id`
+
+	return db.QueryRowx(query, &payment.Type, &payment.OrganisationID, &payment.Attributes).Scan(&payment.ID)
+}
+
+// PUT, PATCH /payments/:id
+func ReplacePayment(payment *Payment) error {
+	query := `
+		UPDATE
+			payments
+		SET
+			version = :version,
+			type_id = (SELECT id FROM payment_types WHERE type = :type),
+			organisation_id = :organisation_id,
+			attributes = :attributes
+		WHERE
+			id = :id AND version = :version - 1`
+
+	_, err := db.NamedExec(query, &payment)
+
+	return err
+}
+
+// DELETE /payments/:id
+func ErasePayment(id string) error {
+	query := `
+		DELETE FROM
+			payments
+		WHERE
+			id = $1`
+
+	_, err := db.Exec(query, id)
+
+	return err
 }
--- handlers.go
+++ handlers.go
@@ -1,45 +1,176 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+
+	"github.com/imdario/mergo"
 )
+
+type Response struct {
+	Data  interface{} `json:"data"`
+	Links `json:"links"`
+}
+
+type Links struct {
+	Self string `json:"self"`
+}
+
+type Error struct {
+	Message string `json:"message"`
+}
+
+// Response boilerplate code.
+func serve(w http.ResponseWriter, code int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(code)
+	if code == 204 || code >= 400 {
+		json.NewEncoder(w).Encode(&Error{Message: http.StatusText(code)})
+	} else {
+		json.NewEncoder(w).Encode(data)
+	}
+}
 
 // GET /payments - read all payments.
 func ReadPayments(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	log.Print(r.Method, r.URL.Path)
-	fmt.Fprint(w, "Welcome!\n")
+	var status int
+	var response *Response
+	payments := []Payment{}
+	if err := QueryPayments(&payments); err != nil {
+		log.Print(err)
+		status = http.StatusInternalServerError
+	} else {
+		status = http.StatusOK
+		response = &Response{
+			Data:  &payments,
+			Links: Links{Self: "https://api.test.form3.tech/v1/payments"},
+		}
+	}
+	serve(w, status, response)
 }
 
 // GET /payments/:id - read a specific payment.
 func ReadPayment(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	log.Print(r.Method, r.URL.Path)
-	fmt.Fprint(w, "Welcome!\n")
+	var status int
+	var response *Response
+	var payment Payment
+	if err := QueryPayment(&payment, ps.ByName("id")); err != nil {
+		log.Print(err)
+		switch err {
+		case sql.ErrNoRows:
+			status = http.StatusNotFound
+		default:
+			status = http.StatusInternalServerError
+		}
+	} else {
+		status = http.StatusOK
+		response = &Response{
+			Data:  &payment,
+			Links: Links{Self: "https://api.test.form3.tech/v1/payments"},
+		}
+	}
+	serve(w, status, response)
 }
 
 // POST /payments - create new payment.
 func CreatePayment(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	log.Print(r.Method, r.URL.Path)
-	fmt.Fprint(w, "Welcome!\n")
+	var status int
+	var response *Response
+	var payment Payment
+	if err := json.NewDecoder(r.Body).Decode(&payment); err != nil {
+		log.Print(err)
+		status = http.StatusBadRequest
+	} else if err := StorePayment(&payment); err != nil {
+		log.Print(err)
+		status = http.StatusInternalServerError
+	} else {
+		status = http.StatusCreated
+		response = &Response{
+			Data:  &payment,
+			Links: Links{Self: "https://api.test.form3.tech/v1/payments"},
+		}
+	}
+	serve(w, status, response)
 }
 
 // PUT /payments/:id - replace payment completely.
 func UpgradePayment(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	log.Print(r.Method, r.URL.Path)
-	fmt.Fprint(w, "Welcome!\n")
+	var status int
+	var response *Response
+	var payload, payment Payment
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		log.Print(err)
+		status = http.StatusBadRequest
+	} else if err := QueryPayment(&payment, ps.ByName("id")); err != nil {
+		log.Print(err)
+		switch err {
+		case sql.ErrNoRows:
+			status = http.StatusNotFound
+		default:
+			status = http.StatusInternalServerError
+		}
+	} else {
+		payload.ID = payment.ID
+		payload.Version = payment.Version + 1
+		if err := ReplacePayment(&payload); err != nil {
+			log.Print(err)
+			status = http.StatusInternalServerError
+		} else {
+			status = http.StatusOK
+			response = &Response{
+				Data:  &payload,
+				Links: Links{Self: "https://api.test.form3.tech/v1/payments"},
+			}
+		}
+	}
+	serve(w, status, response)
 }
 
 // PATCH /payments/:id - update payment partially.
 func UpdatePayment(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	log.Print(r.Method, r.URL.Path)
-	fmt.Fprint(w, "Welcome!\n")
+	var status int
+	var response *Response
+	var payload, payment Payment
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		log.Print(err)
+		status = http.StatusBadRequest
+	} else if err := QueryPayment(&payment, ps.ByName("id")); err != nil {
+		log.Print(err)
+		switch err {
+		case sql.ErrNoRows:
+			status = http.StatusNotFound
+		default:
+			status = http.StatusInternalServerError
+		}
+	} else {
+		mergo.Merge(&payload, payment)
+		payload.Version++
+		if err := ReplacePayment(&payload); err != nil {
+			log.Print(err)
+			status = http.StatusInternalServerError
+		} else {
+			status = http.StatusOK
+			response = &Response{
+				Data:  &payload,
+				Links: Links{Self: "https://api.test.form3.tech/v1/payments"},
+			}
+		}
+	}
+	serve(w, status, response)
 }
 
 // DELETE /payments/:id - delete payment by ID.
 func DeletePayment(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	log.Print(r.Method, r.URL.Path)
-	fmt.Fprint(w, "Welcome!\n")
+	var status int
+	if err := ErasePayment(ps.ByName("id")); err != nil {
+		log.Print(err)
+		status = http.StatusInternalServerError
+	} else {
+		status = http.StatusNoContent
+	}
+	serve(w, status, nil)
 }
